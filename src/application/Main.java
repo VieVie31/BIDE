f656fee66df73a98dfe4b9c1a2f1f0b7734a9c9f
VieVie31 <orissermaroix@gmail.com>
2016-02-14 14:53:47 +0100

    Debut d'interface graphique ajoutée...

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,26 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.*;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		Parent root;
+		Scene scene;
+		
+		root = FXMLLoader.load(getClass().getResource("IDE.fxml"));
+		scene = new Scene(root);
+		
+		primaryStage.setTitle("BIDE");
+		primaryStage.setMinWidth(600);
+		primaryStage.setMinHeight(400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
